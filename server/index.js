ce8b2a0d0293384eb0a3b4ae8ab9f55006926250
PZ <wikiwoo@PZ.local>
2023-07-27 22:55:22 +0300

    back + front

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+// app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://petrozabila:9249TcefLtoZd0SI@portfolio.rt0yy3i.mongodb.net';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,19 +1,19 @@
 import express from 'express';
 
-import { getComments, getCommentsBySearch, getCommentsByCreator, getComment, createComment, updateComment, likeComment, commentComment, deleteComment } from '../controllers/comments.js';
+// import { getComments, getCommentsBySearch, getCommentsByCreator, getComment, createComment, updateComment, likeComment, commentComment, deleteComment } from '../controllers/comments.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
-router.get('/creator', getCommentsByCreator);
-router.get('/search', getCommentsBySearch);
-router.get('/', getComments);
-router.get('/:id', getComment);
+// router.get('/creator', getCommentsByCreator);
+// router.get('/search', getCommentsBySearch);
+// router.get('/', getComments);
+// router.get('/:id', getComment);
 
-router.post('/', auth,  createComment);
-router.patch('/:id', auth, updateComment);
-router.delete('/:id', auth, deleteComment);
-router.patch('/:id/likePost', auth, likeComment);
-router.post('/:id/commentPost', commentComment);
+// router.post('/', auth,  createComment);
+// router.patch('/:id', auth, updateComment);
+// router.delete('/:id', auth, deleteComment);
+// router.patch('/:id/likePost', auth, likeComment);
+// router.post('/:id/commentPost', commentComment);
 
-export default router;s
+export default router;
