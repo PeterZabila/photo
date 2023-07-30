1972951120cc507c0f85ea798af587e850b4d1ba
PZ <wikiwoo@PZ.local>
2023-07-30 23:51:58 +0300

    back + front

--- server/index.js
+++ server/index.js
@@ -2,20 +2,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 // app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://petrozabila:9249TcefLtoZd0SI@portfolio.rt0yy3i.mongodb.net';
+const CONNECTION_URL = 'mongodb+srv://petrozabila:qwerty1988@portfolio.rt0yy3i.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
